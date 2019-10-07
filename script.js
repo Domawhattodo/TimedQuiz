6fca6e7d470cc90e199fdcd255b733ecbeeff4ac
gdpettus <gabe.pettus@gmail.com>
2019-10-06 21:18:04 -0600

    fixed timer countdown. including penelty. Added user input bones to game over

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let highscoreDiv = document.querySelector("#highscore");
 let gameTimerEl = document.querySelector("#gameTimer");
 let quesTimerEl = document.querySelector("#quesTimer");
 let mainEl = document.querySelector("#details");
+let timerTab = document.querySelector("#timers");
 
 
 // let questionEl = document.querySelector("#question")
@@ -24,11 +25,12 @@ var questionInterval;
 // draw instruction
 init();
 
-var startButton = document.querySelector("#startQuiz");
+// var startButton = document.querySelector("#startQuiz");
 
 // function to display instructions
 function init() {
   clearDetails();
+  reset();
   // creates Heading element for main page
   let heading = document.createElement("p");
   heading.setAttribute("id", "main-heading");
@@ -37,16 +39,19 @@ function init() {
   // creates elements with the instructions for the game
   let instructions = document.createElement("p");
   instructions.setAttribute("id", "instructions");
-  instructions.textContent = " You will have 15 seconds to answer each question. If you answer correctly you will score points. The quicker you answer the more points you will score. If you score incorrectly you will not lose points, but you will be penalized time."; 
+  instructions.textContent = " You will have 5 seconds to answer each question. If you answer correctly you will score points. The quicker you answer the more points you will score. If you score incorrectly you will not lose points, but you will be penalized time."; 
 
   // creates button to start the game
   let startQuiz = document.createElement("button");
   startQuiz.setAttribute("id", "startQuiz");
+  startQuiz.setAttribute("class", "btn btn-secondary");
   startQuiz.textContent= "Start Quiz";
 
   mainEl.appendChild(heading);
   mainEl.appendChild(instructions);
   mainEl.appendChild(startQuiz);
+
+  startQuiz.addEventListener("click", playQuiz);
 }
 
 // function to clear details element of all children
@@ -54,32 +59,42 @@ function clearDetails() {
   mainEl.innerHTML = "";
 }
 
+function reset() {
+  var score = 0;
+
+  gameDuration = 0;
+  gameSecElapsed = 0;
+  gameInterval;
+
+  questionDuration = 15;
+  questionSecElapsed = 0;
+  questionInterval;
+}
+
 //start game
 function playQuiz() {
   if (test) { console.log("--- playQuiz ---"); }
   // select quiz randomize questions
-  quiz = setUpQuestions(questions);
-  gameDuration = quiz.length * 15;
   
-  // Start Game timer here
+  quiz = setUpQuestions(questions);
+
+  // displays timers
+  timerTab.setAttribute("style", "visibility: visible;");
+
+  // Start timers here
+  gameDuration = quiz.length * 15;
+  console.log("duration g,q:",gameDuration,questionDuration);
+
   startGameTimer();
   renderTime();
 
-  // interact through randized questions
-  // for ( i=0; i<quiz.length; i++ ) {
-    //displays question 
-    presentQuestion(quiz);
-    // return;
-  // }
-
-  // if (test) { console.log("quiz length: " + quiz.length); }
-  // if (test) { console.log("quiz[2].title: " + quiz[2].title); }
-  
+  //go to first question
+  presentQuestion();
 }
 
 // function to get random question out of array
 function setUpQuestions(arr) {
-  // if (test) {console.log("--- setUpQuestions ---");}
+  if (test) {console.log("--- setUpQuestions ---");}
   //TODO get different topic
   // TODO randomize questions
   let ranQuest = [];
@@ -91,27 +106,34 @@ function setUpQuestions(arr) {
 }
 
 // function to redraw screen with  question 
-function presentQuestion(reducedQuiz) {
+function presentQuestion() {
   if (test) {console.log("--- presentQuestion ---");}
   // if (test) {console.log("cur.choices[i] " + cur.choices);}
 
+  //reset time allows to answer question
+  questionSecElapsed = 0;
+
   // checks for no more questions and exits
-  if ( reducedQuiz.length === 0 ) {
+  if ( quiz.length === 0 ) {
     endOfGame();
     return;
   }
-  //sets current object (question) by pulling off of reducedQuiz
-  cur = reducedQuiz.pop();
-  // console.log("reducedquiz",reducedQuiz);
+
+  // call question timer here
+  // reduceQUiz global
+
+  //sets current object (cur - question) by pulling out of reducedQuiz array leaving the remaining quetions in the array
+  curQuestion = quiz.pop();
 
   //clears html to draw questions
   clearDetails();
    
+  // add question to screen
   //build out display for new item
   let question = document.createElement("h1");
   // adds data value
-  question.setAttribute("question", cur.title);
-  question.textContent = cur.title;
+  question.setAttribute("question", curQuestion.title);
+  question.textContent = curQuestion.title;
   mainEl.appendChild(question)
 
   // create list as container to listen for answers
@@ -120,28 +142,27 @@ function presentQuestion(reducedQuiz) {
   mainEl.appendChild(choiceBox);
 
   //adds answers to screen
-  for( let i=0; i<cur.choices.length; i++ ) {
+  for( let i=0; i<curQuestion.choices.length; i++ ) {
     // creates variable for each choice item
     let listChoice = document.createElement("li");
     // adds data value
-    listChoice.setAttribute("choice-value", cur.choices[i]);
-    listChoice.textContent = cur.choices[i];
+    listChoice.setAttribute("choice-value", curQuestion.choices[i]);
+    listChoice.textContent = curQuestion.choices[i];
     //add choice to page
     choiceBox.appendChild(listChoice)
   }
 
-  if (test) { console.log("cur",cur);}
-  // set question counter counting down
-  startQuestionTimer();
+  // if (test) { console.log("cur", curQuestion);}
+
   // get answer from user
   // using the anymous function delays the invocation of the scoreAnswer
   choiceBox.addEventListener("click", function (){
-    scoreAnswer(cur,reducedQuiz);
+    scoreAnswer(curQuestion);
   });
   // calls for the next questions
 }
 
-function scoreAnswer(cur,reducedQuiz) {
+function scoreAnswer(cur) {
   if (test) { console.log("--- scoreAnswer ---");}
  // ensure that the event on the li
   var e = event.target;
@@ -150,60 +171,49 @@ function scoreAnswer(cur,reducedQuiz) {
     // if (selectedItemm === quiz.)
     // if (test) { console.log("check quiz " + quiz.length); }
     // if (test) { console.log("selectedItem quiz " + selectedItem); }
-    if (test) { console.log("selectedItem cur " , cur.answer); }
+    // if (test) { console.log("selectedItem cur " , cur.answer); }
     if ( selectedItem === cur.answer ) {
-      if (test) { console.log("correct answer");}
+      // if (test) { console.log("correct answer");}
       score += questionDuration - questionSecElapsed;
       //TODO display correct
       //TODO music 
     } else {
-      if (test) { console.log("wrong answer");}
+      // if (test) { console.log("wrong answer");}
+      //penelty for being wrong
+      gameDuration -= 10;
+      //TODO let user know they suck
     }
-    presentQuestion(reducedQuiz);
+    presentQuestion();
   }
-    // grabs data index value of property of parent if clicked on botton
-    // let index = element.parentElement.getAttribute("data-index");
-    //splice deletes element out of array
-    // todos.splice(index,1);
-    // console.log("new todos: " + todos);
-    // renderTodos();
- // conpare the answer with the corrent answer
- //add point if correct
- // subtract time if not  
-
- // present next question 
 }
-
 
 // function to set time for game timer
 function setGameTime() {
   if (test) { console.log("--- setGameTime ---"); }
   if (test) { console.log("gameDuration " + gameDuration); }
-
   clearInterval(gameInterval);
   gameSeconds = gameDuration;
 }
 
-// function to set time question timer
-function setQuestionTime() {
-  clearInterval(questionInterval);
-  questionDuration = 15;
-}
 
 function renderTime() {
-  // if (test) { console.log(" --- renderTime --- "); }
-  // if (test) { console.log("gameSecElapsed " + gameSeconds); }
+  if (test) { console.log(" --- renderTime --- "); }
+  // if (test) { console.log("gameSecElapsed " + gameSecElapsed); }
   // if (test) { console.log("gameDuration " + gameDuration); }
-  // if (test) { console.log("questionSecElapsed " + questionSecElapsed); }
   // if (test) { console.log("questionDuration " + questionDuration); }
 
   gameTimerEl.textContent = gameDuration - gameSecElapsed;
   quesTimerEl.textContent = questionDuration - questionSecElapsed;
-  if ( (questionDuration - questionSecElapsed) === 0 ) {
-     endOfGame();
-  }
-  if ( (gameDuration - gameSecElapsed) === 0 ) {
-     endOfGame();
+
+  if ( (questionDuration - questionSecElapsed) < 1 ) {
+    // game penelty for letting timer run out
+    gameDuration -= 10;
+    console.log("too slow");
+    presentQuestion();
+  } 
+
+  if ( (gameDuration - gameSecElapsed) < 1 ) {
+   endOfGame();
   }
 }
 
@@ -213,14 +223,6 @@ function startGameTimer () {
 
   gameInterval = setInterval(function() {
     gameSecElapsed++; 
-    renderTime();
-  }, 1000);
-}
-
-function startQuestionTimer () {
-  setQuestionTime();
-
-  questionInterval = setInterval(function() {
     questionSecElapsed++; 
     renderTime();
   }, 1000);
@@ -230,8 +232,7 @@ function stopTime() {
   if (test) { console.log("--- stopTime --- ");}
   gameSeconds = 0;
   questionSeconds = 0;
-  setGameTime();
-  setQuestionTime();
+  clearInterval(gameInterval);
 }
 
 // function of end of game
@@ -239,14 +240,51 @@ function endOfGame() {
   if (test) { console.log("--- endOfGame ---"); }
   stopTime();
   clearDetails();
+
+  timerTab.setAttribute("style", "visibility: hidden;");
+
+  let heading = document.createElement("p");
+  heading.setAttribute("id", "main-heading");
+  heading.textContent = "GAME OVER - I hope you have enjoyed this";
+
+  // creates elements with the instructions for the game
+  let instructions = document.createElement("p");
+  instructions.setAttribute("id", "instructions");
+  instructions.textContent = " Your score is " + score; 
+
+  // creates button to start the game
+  let playAgain = document.createElement("button");
+  playAgain.setAttribute("id", "playAgain");
+  playAgain.setAttribute("class", "btn btn-secondary");
+  playAgain.textContent = "Play again";
+
+  // creates input for user to add initials
+  let par = document.createElement("p");
+
+  let initialsLabel = document.createElement("label");
+  initialsLabel.setAttribute("for","userInitials");
+  initialsLabel.textContent = "Enter Initials: ";
+
+  let initialsInput = document.createElement("input");
+  initialsInput.setAttribute("id","userInitials");
+  initialsInput.setAttribute("name","userInitials");
+  initialsInput.setAttribute("minlength","3");
+  initialsInput.setAttribute("maxlength","3");
+  initialsInput.setAttribute("size","3");
+
+
+  mainEl.appendChild(heading);
+  mainEl.appendChild(instructions);
+  mainEl.appendChild(initialsLabel);
+  mainEl.appendChild(initialsInput);
+  mainEl.appendChild(par);
+  mainEl.appendChild(playAgain);
+
+  score = 0;
+  playAgain.addEventListener("click", init);
 }
 
 // save high score to browser memery
 function saveScore() {
 
 }
-
-// eventListener for start of play
-startButton.addEventListener("click", playQuiz);
-
-// event listener to question answering
